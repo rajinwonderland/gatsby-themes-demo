8229aa52fc78c6a5994479bc685608912cccfb3d
rajinwonderland <raj@novvuminc.com>
2019-04-02 15:55:50 -0700

    Updates

--- examples/color/src/components/mdx-layout.js
+++ examples/color/src/components/mdx-layout.js
@@ -2,7 +2,8 @@ import React from 'react';
 import styled from '@emotion/styled';
 
 const Base = styled.div({
-	flex: '1 1 auto'
+	flex: '1 1 auto',
+	padding: '1rem'
 });
 
 export default (props) => <Base>{props.children}</Base>;
